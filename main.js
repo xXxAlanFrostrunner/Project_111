b76ee16cd1cca8c1dbac6b2be3645b36fdbcab54
xXxAlanFrostrunner <106028067+xXxAlanFrostrunner@users.noreply.github.com>
2023-06-11 15:01:09 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+});
+
+
+Webcam.attach( '#camera' );
+
+
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>'
+    })
+}
+
+console.log('ml5 version:', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/q8Fa6tpPO/model.json', modelLoaded)
+
+function modelLoaded() {
+    console.log('Model Loaded!')
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data_1 = "The first prediction is" + prediction_1;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1);
+    synth.speak(utterThis);
+}
+
